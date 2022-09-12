ecda9824d3fe8d2e3c3c3820da77fd9825e9ef87
Sanford Lin <craftedswift@gmail.com>
2022-09-12 11:56:34 -0400

    Finished TODO: Added linSanfo method in IntroLab and added relevant test in IntroLabTest

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -24,10 +24,10 @@ public class IntroLabTest {
         String actual = IntroLab.exampleStudent();
         assertEquals(expected, actual);
     }
-
-    /* TODO: Write a test case for the method you wrote in IntroLab.
-     *       If done properly, you should be able to run IntroLabTest and see
-     *       the test results.
-     * As a reference, we've included testExampleStudent above.
-     */
+    @Test (timeout = 10)
+    public void testLinSanfo() {
+        String expected = "Hey! My name is Sanford! I like dogs more than cats! What am I doing here?";
+        String actual = IntroLab.linSanfo();
+        assertEquals(expected, actual);
+    }
 }
